c31071f633cc5dc3b66abd8d309bc8a3db0b5d5a
SAMUS <usa06565@gmail.com>
2025-05-03 15:50:27 -0600

    conexiones

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,32 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [respuesta, setRespuesta] = useState(null)
+
+  const enviarAnalizador = async () => {
+    const textarea = document.querySelector(".txt-confi")
+    const texto = textarea.value
+
+    try {
+      const response = await fetch("http://localhost:4000/manual", {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+        },
+        body: texto,
+      })
+
+      const data = await response.json()
+      console.log("Respuesta analizador:", data)
+      setRespuesta(data.message || 'Configuración enviada.')
+    } catch (error) {
+      console.error("Error al enviar al analizador:", error)
+      setRespuesta("Error al conectar con el backend.")
+    }
+  }
 
   return (
     <>
-    <div className='page'>
-      <nav className='navbar-orga'>
-        <img src="src/img/mina-terrestre.png" alt="" className='logo'/>
-        <h2>BUSCAMINAS</h2>
-      </nav>
-      <div className='txts'>
-        <textarea name="console" id="" className='txt-confi'>
-        </textarea>
-        <div className='btns'>
-          <button className='btn-confi'>Configurar</button>
-          <div class="page_img">
-            <a href="tu-enlace.html" class="image-link">
-              <img src="src/img/tocar.png" alt="" className='imagebtn'/>
-            </a>
-            <a href="tu-enlace.html" class="image-link">
-              <img src="src/img/close_1828527.png" alt="" className='imagebtn'/>
-            </a>
+      <div className='page'>
+        <nav className='navbar-orga'>
+          <img src="src/img/mina-terrestre.png" alt="" className='logo' />
+          <h2>BUSCAMINAS</h2>
+        </nav>
+
+        <div className='txts'>
+          <textarea name="console" className='txt-confi'></textarea>
+
+          <div className='btns'>
+            <button className='btn-confi' onClick={enviarAnalizador}>Configurar</button>
+
+            <div className="page_img">
+              <a href="#" className="image-link">
+                <img src="src/img/tocar.png" alt="" className='imagebtn' />
+              </a>
+              <a href="#" className="image-link">
+                <img src="src/img/close_1828527.png" alt="" className='imagebtn' />
+              </a>
+            </div>
           </div>
-          </div>
+
+          {respuesta && <div className="respuesta">{respuesta}</div>}
         </div>
       </div>
     </>
